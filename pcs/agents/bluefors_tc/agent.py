883b5c2dfb4d631400b9493b9a3bd5a52b402769
Zach Huber <zbh5@cornell.edu>
2023-11-09 15:09:27 -0500

    Adding core functionality for BF TC agent code

--- pcs/agents/bluefors_tc/agent.py
+++ pcs/agents/bluefors_tc/agent.py
@@ -1,2 +1,239 @@
 # Bluefors TC agent
-# To be written!
+
+import argparse
+import threading
+from contextlib import contextmanager
+
+from ocs import ocs_agent, site_config
+from ocs.ocs_twisted import Pacemaker, TimeoutLock
+from twisted.internet import reactor
+
+from pcs.drivers.bluefors_tc import BFTC
+
+class YieldingLock:
+    """
+    Borrowed from SOCS Lakeshore372 Agent on 11/9/2023 - could just import
+    it but want to ensure it doesn't get changed in a future SOCS release
+    without us knowing it.
+    
+    A lock protected by a lock.  This braided arrangement guarantees
+    that a thread waiting on the lock will get priority over a thread
+    that has just released the lock and wants to reacquire it.
+
+    The typical use case is a Process that wants to hold the lock as
+    much as possible, but occasionally release the lock (without
+    sleeping for long) so another thread can access a resource.  The
+    method release_and_acquire() is provided to make this a one-liner.
+
+    """
+
+    def __init__(self, default_timeout=None):
+        self.job = None
+        self._next = threading.Lock()
+        self._active = threading.Lock()
+        self._default_timeout = default_timeout
+
+    def acquire(self, timeout=None, job=None):
+        if timeout is None:
+            timeout = self._default_timeout
+        if timeout is None or timeout == 0.:
+            kw = {'blocking': False}
+        else:
+            kw = {'blocking': True, 'timeout': timeout}
+        result = False
+        if self._next.acquire(**kw):
+            if self._active.acquire(**kw):
+                self.job = job
+                result = True
+            self._next.release()
+        return result
+
+    def release(self):
+        self.job = None
+        return self._active.release()
+
+    def release_and_acquire(self, timeout=None):
+        job = self.job
+        self.release()
+        return self.acquire(timeout=timeout, job=job)
+
+    @contextmanager
+    def acquire_timeout(self, timeout=None, job='unnamed'):
+        result = self.acquire(timeout=timeout, job=job)
+        if result:
+            try:
+                yield result
+            finally:
+                self.release()
+        else:
+            yield result
+
+
+class Bluefors_TC_Agent:
+    """Agent to connect to a single Bluefors Temperature Controller device.
+    
+    """
+    
+    def __init__(self, agent, name, ip):
+
+        # self._acq_proc_lock is held for the duration of the acq Process.
+        # Tasks that require acq to not be running, at all, should use
+        # this lock.
+        self._acq_proc_lock = TimeoutLock()
+
+        # self._lock is held by the acq Process only when accessing
+        # the hardware but released occasionally so that (short) Tasks
+        # may run.  Use a YieldingLock to guarantee that a waiting
+        # Task gets activated preferentially, even if the acq thread
+        # immediately tries to reacquire.
+        self._lock = YieldingLock(default_timeout=5)
+
+        self.name = name
+        self.ip = ip
+        self.module = None
+        self.thermometers = []
+
+        self.log = agent.log
+        self.initialized = False
+        self.take_data = False
+
+        self.agent = agent
+        # Registers temperature feeds
+        agg_params = {
+            'frame_length': 10 * 60  # [sec]
+        }
+        self.agent.register_feed('temperatures',
+                                 record=True,
+                                 agg_params=agg_params,
+                                 buffer_time=1)
+
+    @ocs_agent.param('auto_acquire', default=False, type=bool)
+    @ocs_agent.param('acq_params', type=dict, default=None)
+    def init_bftc(self, session, params=None):
+        """init_bftc(auto_acquire=False, acq_params=None)
+        
+        **Task** - Perform first time setup of the communication to the BFTC.
+        
+        Parameters:
+            auto_acquire (bool, optional): Default is False. Starts data
+                acquisition after initialization if True.
+            acq_params (dict, optional): Params to pass to acq process if
+                auto_acquire is True.
+        """
+
+        if params is None:
+            params = {}
+        if self.initialized and not params.get('force', False):
+            self.log.info("Lakeshore already initialized. Returning...")
+            return True, "Already initialized"
+
+        with self._lock.acquire_timeout(job='init') as acquired1, \
+                self._acq_proc_lock.acquire_timeout(timeout=0., job='init') \
+                as acquired2:
+            if not acquired1:
+                self.log.warn(f"Could not start init because "
+                              f"{self._lock.job} is already running")
+                return False, "Could not acquire lock"
+            if not acquired2:
+                self.log.warn(f"Could not start init because "
+                              f"{self._acq_proc_lock.job} is already running")
+                return False, "Could not acquire lock"
+
+            session.set_status('running')
+            
+            try:
+                self.module = BFTC(self.ip)
+            except ConnectionError:
+                self.log.error("Could not connect to the BF TC. Exiting.")
+                reactor.callFromThread(reactor.stop)
+                return False, 'BF TC initialization failed'
+            except Exception as e:
+                self.log.error(f"Unhandled exception encountered: {e}")
+                reactor.callFromThread(reactor.stop)
+                return False, 'BF TC initialization failed'
+
+            print("Initialized BF TC module: {!s}".format(self.module))
+            session.add_message("BF TC initilized with ID: %s" % self.module.id)
+
+            self.thermometers = [channel.name for channel in self.module.channels]
+            
+            self.initialized = True
+
+        # Start data acquisition if requested
+        if params.get('auto_acquire', False):
+            self.agent.start('acq', params.get('acq_params', None))
+
+        return True, 'BF TC initialized.'
+    
+    def acq(self, session, params=None):
+        pass
+
+    def _stop_acq(self, session, params=None):
+        """
+        Stops acq process.
+        """
+        if self.take_data:
+            session.set_status('stopping')
+            self.take_data = False
+            return True, 'requested to stop taking data.'
+        else:
+            return False, 'acq is not currently running'
+
+def make_parser(parser=None):
+    """Build the argument parser for the Agent. Allows sphinx to automatically
+    build documentation based on this function.
+
+    """
+    if parser is None:
+        parser = argparse.ArgumentParser()
+
+    # Add options specific to this agent.
+    pgroup = parser.add_argument_group('Agent Options')
+    pgroup.add_argument('--ip-address')
+    pgroup.add_argument('--serial-number')
+    pgroup.add_argument('--mode', type=str, default='acq',
+                        choices=['idle', 'init', 'acq'],
+                        help="Starting action for the Agent.")
+
+    return parser
+
+
+def main(args=None):
+    # For logging
+    txaio.use_twisted()
+    txaio.make_logger()
+
+    # Start logging
+    txaio.start_logging(level=os.environ.get("LOGLEVEL", "info"))
+
+    parser = make_parser()
+    args = site_config.parse_args(agent_class='BFTCAgent',
+                                  parser=parser,
+                                  args=args)
+
+    # Automatically acquire data if requested (default)
+    init_params = False
+    if args.mode == 'init':
+        init_params = {'auto_acquire': False,
+                       'acq_params': {}}
+    elif args.mode == 'acq':
+        init_params = {'auto_acquire': True}
+
+    # Interpret options in the context of site_config.
+    print('I am in charge of device with serial number: %s' % args.serial_number)
+
+    agent, runner = ocs_agent.init_site_agent(args)
+
+    bftc_agent = Bluefors_TC_Agent(agent, args.serial_number, args.ip_address)
+
+    agent.register_task('init_bftc', bftc_agent.bftc,
+                        startup=init_params)
+    agent.register_process('acq', bftc_agent.acq, bftc_agent._stop_acq)
+    # And many more to come...
+
+    runner.run(agent, auto_reconnect=True)
+
+
+if __name__ == '__main__':
+    main()
+    

c3f186aafddce0899382d987e4a68794c24c79e6
Zach Huber <78050507+zhuber21@users.noreply.github.com>
2025-03-26 16:48:00 -0400

    Adding BFTC to plugin.py

--- pcs/plugin.py
+++ pcs/plugin.py
@@ -3,4 +3,5 @@ agents = {
     'LS325Agent': {'module': 'pcs.agents.lakeshore325.agent', 'entry_point': 'main'},
     'RaritanAgent': {'module': 'pcs.agents.raritan_pdu.agent', 'entry_point': 'main'},
     'ACUAgent': {'module': 'pcs.agents.acu_interface.agent', 'entry_point': 'main'},
+    'Bluefors_TC_Agent': {'module': 'pcs.agents.bluefors_tc.agent', 'entry_point': 'main'}
 }

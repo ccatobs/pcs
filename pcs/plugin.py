2ee1f8acafda3d2f1bf6bfe75082b37a5bbdff22
Alicia Middleton <amm542@cornell.edu>
2026-03-05 14:32:59 -0500

    Fixed BFTC agent so it can read out temps.

--- pcs/plugin.py
+++ pcs/plugin.py
@@ -3,5 +3,6 @@ agents = {
     'LS325Agent': {'module': 'pcs.agents.lakeshore325.agent', 'entry_point': 'main'},
     'RaritanAgent': {'module': 'pcs.agents.raritan_pdu.agent', 'entry_point': 'main'},
     'ACUAgent': {'module': 'pcs.agents.acu_interface.agent', 'entry_point': 'main'},
-    'Bluefors_TC_Agent': {'module': 'pcs.agents.bluefors_tc.agent', 'entry_point': 'main'}
+    'Bluefors_TC_Agent': {'module': 'pcs.agents.bluefors_tc.agent', 'entry_point': 'main'},
+    'PfeifferAgent': {'module': 'pcs.agents.pfeiffer_singlegauge.agent', 'entry_point': 'main'}
 }

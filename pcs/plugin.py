89198595fbed19cb3f3a7976f0f1254dae596972
ChrisMyers <c.myers@cornell.edu>
2026-09-24 15:46:36 -0400

    Fixed syntax in plugin.py

--- pcs/plugin.py
+++ pcs/plugin.py
@@ -8,6 +8,6 @@ agents = {
     'DymoAgent': {'module': 'pcs.agents.dymo.agent', 'entry_point': 'main'},
     'TeledyneAgent': {'module': 'pcs.agents.teledyne.agent', 'entry_point': 'main'},
     'PfeifferAgent': {'module': 'pcs.agents.pfeiffer_singlegauge.agent', 'entry_point': 'main'},
-    'Adam_Agent':{'module': 'pcs.agents.adam.agent', 'entry_point': 'main'}
+    'Adam_Agent':{'module': 'pcs.agents.adam.agent', 'entry_point': 'main'},
     'BookbinderAgent':{'module': 'pcs.agents.bookbinder.agent', 'entry_point': 'main'}
 }
